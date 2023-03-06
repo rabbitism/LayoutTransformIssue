e1020589a91d08ca586a1508931264714e818ac4
rabbitism <popmessiah@hotmail.com>
2023-03-06 21:01:47 +0800

    repro issue.

--- AvaloniaApplication1/MainWindow.axaml.cs
+++ AvaloniaApplication1/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Layout;
 
 namespace AvaloniaApplication1;
 
@@ -7,5 +9,17 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (bar.Orientation == Orientation.Horizontal)
+        {
+            bar.Orientation = Orientation.Vertical;
+        }
+        else
+        {
+            bar.Orientation = Orientation.Horizontal;
+        }
     }
 }
